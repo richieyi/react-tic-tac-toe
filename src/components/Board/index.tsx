4bf7b01576712049f3ffb840e35f7a4324378289
Richie Yi <helloworldrichie@gmail.com>
2022-08-24 10:56:23 -0700

    Adjust styling

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from 'react';
 import { isGameWon } from '../../utils';
 
-const emptyBoard = [...Array(3)].map((_) => Array(3).fill('?'));
-const defaultBoard = [...Array(3)].map((_) => Array(3).fill('?'));
 const defaultTurn = 'X';
-
 export type PieceType = 'X' | 'O';
 export type BoardType = string[][];
 
 function Board() {
+  const defaultBoard = [...Array(3)].map((_) => Array(3).fill('?'));
   const [board, setBoard] = useState<BoardType>(defaultBoard);
   const [turn, setTurn] = useState<PieceType>(defaultTurn);
   const [isWon, setIsWon] = useState<boolean>(false);
@@ -23,7 +21,7 @@ function Board() {
     return board.map((row, rowIdx) => {
       return row.map((tile, tileIdx) => (
         <button
-          className="border border-black h-20 w-20"
+          className="border border-black h-20 w-20 text-xl"
           data-testid="board-tile"
           key={`${tile}-${tileIdx}`}
           onClick={() => setPiece(rowIdx, tileIdx, turn)}
@@ -54,15 +52,15 @@ function Board() {
   }
 
   function resetBoard() {
-    setBoard(emptyBoard);
+    setBoard(defaultBoard);
     setTurn(defaultTurn);
     setIsWon(false);
   }
 
   return (
     <div data-testid="board">
-      <div className="flex justify-center">
-        <div className="grid grid-cols-3 m-auto max-w-md py-12">
+      <div className="flex justify-center py-12 m-auto max-w-md">
+        <div className="grid grid-cols-3 border border-black rounded shadow-lg">
           {renderBoard()}
         </div>
       </div>
@@ -77,7 +75,7 @@ function Board() {
         <div className="flex justify-center">
           <button
             data-testid="reset-button"
-            className="border bg-blue-500 text-white rounded px-8 py-2 my-2"
+            className="border bg-blue-500 hover:bg-blue-500/75 shadow-md text-white rounded px-8 py-2 my-2"
             onClick={resetBoard}
           >
             Reset
